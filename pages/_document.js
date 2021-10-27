f542c42b2a638830a8563e440c93c7e141888fbc
harelpanker <harelpanker@gmail.com>
2021-10-27 14:29:22 +0200

    try

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,11 @@ class MyDocument extends Document {
         <Head>
           {/* google fonts */}
           <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='true'
+          />
           <link
             href='https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;700&display=swap'
             rel='stylesheet'
