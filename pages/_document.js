ac9cd6cb03a626c90bd60c9fc869dffbbf84b272
harelpanker <harelpanker@gmail.com>
2021-10-27 10:48:45 +0200

    first commit

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang='en' dir='ltr'>
+        <Head>
+          {/* google fonts */}
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;700&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body className='antialiased bg-gray-50 text-gray-900 font-spartan'>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
